f95a53fdbc5da7fe0c9bf91b5b3071b4bd6241e3
msj <1498218391@qq.com>
2025-04-28 16:15:51 +0800

    feat(arcgis_map): 在点击地图featurelayer地块时，识别地块信息并显示对话框。

--- lib/arcgis_map.dart
+++ lib/arcgis_map.dart
@@ -16,7 +16,7 @@ class MainApp extends StatefulWidget {
 
 class _MainAppState extends State<MainApp> {
   final _mapViewController = ArcGISMapView.createController();
-
+  late FeatureCollectionLayer _featureCollectionLayer; // 保存要素图层
   onMapTap(Offset details) async {
     // 将屏幕坐标转换为地图坐标
     final mapPoint = await PointUtil.screenToWGS84(
@@ -24,9 +24,64 @@ class _MainAppState extends State<MainApp> {
       screenPoint: details,
     );
     if (mapPoint != null) {
-      print('点击位置的地理坐标：');
-      print('经度：${mapPoint.x}');
-      print('纬度：${mapPoint.y}');
+      // print('点击位置的地理坐标：');
+      // print('经度：${mapPoint.x}');
+      // print('纬度：${mapPoint.y}');
+      // 添加地块识别功能
+      // final identifyResults = await _mapViewController.identifyLayers(
+      //   // _featureCollectionLayer, // 替换为你的要素图层变量
+      //   screenPoint: details,
+      //   tolerance: 22, // 点击容差（像素）
+      //   maximumResultsPerLayer: 10,
+      // );
+      // var identifyTotal = 0;
+      // final layerCounts = <String>[];
+      // for (final result in identifyResults) {
+      //   print(result);
+      // }
+      final identifyResult = await _mapViewController.identifyLayer(
+        _featureCollectionLayer, // 替换为你的要素图层变量
+        screenPoint: details,
+        tolerance: 22, // 点击容差（像素）
+      );
+      var attributes;
+
+      if (identifyResult.sublayerResults.isNotEmpty) {
+        attributes =
+            identifyResult.sublayerResults.first.geoElements.first.attributes;
+      }
+      if (attributes != null) {
+        if (!mounted) return;
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('地块信息'),
+              content: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('地块编号: ${attributes["id"]}'),
+                  Text('地块名称: ${attributes["name"]}'),
+                  Text('地块面积: ${attributes["areaComp"]}'),
+                  Text('地块地点: ${attributes["location"]}'),
+                  Text('地块类型: ${attributes["soliType"]}'),
+                ],
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('关闭'),
+                ),
+              ],
+            );
+          },
+        );
+      }
+
+      print(identifyResult);
     } else {
       print('坐标转换失败，可能是地图未完全加载');
     }
@@ -64,15 +119,17 @@ class _MainAppState extends State<MainApp> {
       );
       // 将图层添加到地图中
       map.operationalLayers.addAll([imgLayer, annotationLayer]);
-      var polygonFeatureLayer = await _addFeatureLayer();
-      // map.operationalLayers.add(polygonFeatureLayer);
+      _featureCollectionLayer = await _addFeatureLayer();
+      map.operationalLayers.add(_featureCollectionLayer);
+      // 添加加载完成检查
+
       // 隐藏地图右下角的版权信息
       _mapViewController.isAttributionTextVisible = false;
       _mapViewController.arcGISMap = map;
       // 使用图形管理器添加图形
       final graphicsManager = GraphicsManager();
       graphicsManager.addAllGraphics();
-      _mapViewController.graphicsOverlays.add(graphicsManager.overlay);
+      // _mapViewController.graphicsOverlays.add(graphicsManager.overlay);
 
       _mapViewController.imageOverlays.add(imageOverlay);
       _mapViewController.setViewpoint(
@@ -81,8 +138,11 @@ class _MainAppState extends State<MainApp> {
           // 图片经纬度
           // latitude: 44.856219,
           // longitude: 85.452456,
-          latitude: 44.341538,
-          longitude: 86.008825,
+          // latitude: 44.341538,
+          // longitude: 86.008825,
+          // [85.265665, 45.089596],, featurelayer
+          latitude: 45.089596,
+          longitude: 85.265665,
           scale: levelToScale(15),
         ),
       );
@@ -91,7 +151,7 @@ class _MainAppState extends State<MainApp> {
     }
   }
 
-  Future<FeatureLayer> _addFeatureLayer() async {
+  Future<FeatureCollectionLayer> _addFeatureLayer() async {
     // 定义字段
     final fields = [
       Field.text(name: 'id', alias: '地块编号', length: 20),
@@ -107,20 +167,30 @@ class _MainAppState extends State<MainApp> {
       geometryType: GeometryType.polygon,
       spatialReference: SpatialReference.wgs84,
     );
-    // 设置表的字段
-    featureTable.fields.addAll(fields);
+    // 创建要素集合
+    final featureCollection = FeatureCollection();
+    // 将要素表添加到要素集合中
+    featureCollection.tables.add(featureTable);
 
-    // 创建要素图层
-    final featureLayer = FeatureLayer.withFeatureTable(featureTable);
+    final featureCollectionLayer = FeatureCollectionLayer.withFeatureCollection(
+      featureCollection,
+    );
+
     // 创建多边形几何
     final polygonPoints = <ArcGISPoint>[];
     final latLotList = [
-      [85.452483, 44.856212],
-      [85.452456, 44.856219],
-      [85.458053, 44.856152],
-      [85.458042, 44.849842],
-      [85.45257, 44.850045],
-      [85.452483, 44.856212],
+      [85.265665, 45.089596],
+      [85.264357, 45.08952],
+      [85.262812, 45.089982],
+      [85.261599, 45.089808],
+      [85.261589, 45.088868],
+      [85.259325, 45.088846],
+      [85.257211, 45.088944],
+      [85.257286, 45.092815],
+      [85.264367, 45.092663],
+      [85.265032, 45.091163],
+      [85.265269, 45.090876],
+      [85.265665, 45.089596],
     ];
 
     for (var latLot in latLotList) {
@@ -135,37 +205,39 @@ class _MainAppState extends State<MainApp> {
     // 创建要素
     final feature = featureTable.createFeature(
       attributes: {
-        'id': 'h8WkShXTiULvVmccHZNn4i',
-        'name': '测试地块1',
-        'areaComp': 454.07,
-        'location': '塔城地区 新疆维吾尔自治区塔城地区沙湾市兵团一二一团S20五克高速',
-        'soliType': '盐化灰漠土',
+        'id': 'DNwHaco86QmoggkJxKpsCa',
+        'name': '测试地块2',
+        'areaComp': 355.49,
+        'location': '塔城地区 新疆维吾尔自治区塔城地区沙湾市兵团一二一团',
+        'soliType': '荒漠风沙土',
       },
       geometry: polygon.toGeometry(),
     );
-
     // 添加要素到表中
     await featureTable.addFeature(feature);
 
     // 设置要素图层的渲染器
     final symbol = SimpleFillSymbol(
       style: SimpleFillSymbolStyle.solid,
-      color: Colors.blue.withOpacity(0.3),
+      color: Colors.pink.withOpacity(0.3),
       outline: SimpleLineSymbol(
         style: SimpleLineSymbolStyle.solid,
-        color: Colors.blue,
+        color: Colors.black,
         width: 2,
       ),
     );
-
     final renderer = SimpleRenderer(symbol: symbol);
-    featureLayer.renderer = renderer;
-    return featureLayer;
-    // 添加图层到地图
-    // _mapViewController.operationalLayers.add(featureLayer);
-
-    // 缩放到要素范围
-    // _mapViewController.setViewpointGeometry(polygon);
+    // 将渲染器设置到要素表上，而不是图层
+    featureTable.renderer = renderer;
+    // featureCollectionLayer.renderer = renderer;
+    await featureTable.load();
+    print('要素数量：${featureTable.numberOfFeatures}'); // 验证要素加载
+    if (featureTable.loadStatus == LoadStatus.loaded) {
+      print('要素加载成功');
+    }
+    // featureCollectionLayer.featureCollection.tables.add(featureTable);
+    // featureCollectionLayer.;
+    return featureCollectionLayer;
   }
 
   @override
